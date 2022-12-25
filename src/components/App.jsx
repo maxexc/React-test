163360efe8f09142979330db0945897d4997e143
Maksym Solomko <max.exc7@gmail.com>
2022-12-25 21:35:33 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,23 @@
+// import { useState, useEffect } from "react";
+
+// const App = () => {
+//   const [value, setValue] = useState(0);
+
+//   useEffect(() => {
+//     document.title = `You clicked ${value} times`;
+//   });
+
+//   return (
+//     <div>
+//       <p>You clicked {value} times</p>
+//       <button onClick={() => setValue(value + 1)}>Click me</button>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+
 import React, { Component } from "react";
 import shortid from 'shortid';
 import Container from "./Container";
